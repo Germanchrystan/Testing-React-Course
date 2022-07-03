a928bc297894a0381ff607c984934f50c8cf4d72
Germanchrystan <germanchrystan@gmail.com>
2022-07-03 17:57:51 -0300

    Test working

--- Repos/02-sundaes-on-demand/src/pages/entry/tests/options.test.jsx
+++ Repos/02-sundaes-on-demand/src/pages/entry/tests/options.test.jsx
@@ -5,7 +5,7 @@ import Options from './../Options';
 test('displays images for each scoop option from server', async() => {
     render(<Options optionType="scoops" />)
     //find images
-    const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i }); // Name of image = alt prop
+    const scoopImages = await screen.findAllByRole('img ', { name: /scoop$/i }); // Name of image = alt prop
     expect(scoopImages).toHaveLength(2)
 
     // confirm alt text of images
