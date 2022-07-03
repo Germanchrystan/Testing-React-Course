29613aabd716fa67b2b6080ad9324b5a30c1124d
Germanchrystan <germanchrystan@gmail.com>
2022-07-03 17:34:41 -0300

    Options not Working

--- Repos/02-sundaes-on-demand/src/pages/entry/Options.jsx
+++ Repos/02-sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ScoopOption from './ScoopOption';
 import Row from 'react-bootstrap'
 
--- Repos/02-sundaes-on-demand/src/pages/entry/ScoopOption.jsx
+++ Repos/02-sundaes-on-demand/src/pages/entry/ScoopOption.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Col from 'react-bootstrap';
 
 export default function ScoopOption({ name, imagePath }){
